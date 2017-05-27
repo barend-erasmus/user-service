71854ff1ce3bca4888e30c3faee45259b695e281
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-05-27 08:38:19 +0200

    lint fixes

--- src/models/user-role.ts
+++ src/models/user-role.ts
@@ -6,7 +6,7 @@ export class UserRole {
     public static mapUserRole(x: UserRole): UserRole {
         return new UserRole(x.id, x.name, x.permissions.map((y) => UserPermission.mapUserPermission(y)));
     }
-    
+
     constructor(public id: string, public name: string, public permissions: UserPermission[]) {
 
     }
