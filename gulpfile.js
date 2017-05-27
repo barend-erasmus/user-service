f3852600efebd70641e1b3326ad174eef4126ad6
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-05-27 21:48:31 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var rename = require("gulp-rename");
 var GulpSSH = require('gulp-ssh');
 var sequence = require('run-sequence');
 var argv = require('yargs').argv;
+var merge = require('merge-stream');
+var node_ssh = require('node-ssh')
 
 // Compiles typescript files
 gulp.task('compile:ts', function () {
@@ -48,7 +50,7 @@ gulp.task('build:dev', function (done) {
     sequence('clean', 'compile:ts', 'copy:package.json', done);
 });
 
-gulp.task('publish', function () {
+gulp.task('publish:source', function () {
     var config = {
         host: argv.host,
         port: 22,
@@ -64,4 +66,103 @@ gulp.task('publish', function () {
     return gulp
         .src(['./dist/**'])
         .pipe(gulpSSH.dest(argv.dest));
-})
+});
+
+gulp.task('publish:modules', function (done) {
+
+    var ssh = new node_ssh();
+
+    ssh.connect({
+        host: argv.host,
+        username: argv.username,
+        password: argv.password
+    }).then(function () {
+        ssh.execCommand('npm install', { cwd: `/opt/${argv.service}`}).then(function (result) {
+            ssh.dispose();
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    }).catch(function (err) {
+        done(err);
+    });
+});
+
+gulp.task('publish:dockerfile', function () {
+    var config = {
+        host: argv.host,
+        port: 22,
+        username: argv.username,
+        password: argv.password
+    };
+
+    var gulpSSH = new GulpSSH({
+        ignoreErrors: false,
+        sshConfig: config
+    });
+
+    return gulp
+        .src(['./Dockerfile'])
+        .pipe(gulpSSH.dest(argv.dest));
+});
+
+gulp.task('docker:stop', function (done) {
+    var ssh = new node_ssh();
+
+    ssh.connect({
+        host: argv.host,
+        username: argv.username,
+        password: argv.password
+    }).then(function () {
+        ssh.execCommand(`docker stop ${argv.service}`).then(function (result) {
+            return ssh.execCommand(`docker rm ${argv.service}`);
+        }).then(function (result) {
+            ssh.dispose();
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    }).catch(function (err) {
+        done(err);
+    });
+});
+
+gulp.task('docker:build', function (done) {
+    var ssh = new node_ssh();
+
+    ssh.connect({
+        host: argv.host,
+        username: argv.username,
+        password: argv.password
+    }).then(function () {
+        ssh.execCommand(`docker build --no-cache -t ${argv.service} /opt/${argv.service}`).then(function (result) {
+            return ssh.execCommand(`docker run -d -p 8080:3000 --name ${argv.service} --link user-service-db:mongo -t ${argv.service}`);
+        }).then(function (result) {
+            ssh.dispose();
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    }).catch(function (err) {
+        done(err);
+    });
+});
+
+gulp.task('docker:start', function (done) {
+    var ssh = new node_ssh();
+
+    ssh.connect({
+        host: argv.host,
+        username: argv.username,
+        password: argv.password
+    }).then(function () {
+        ssh.execCommand(`docker start ${argv.service}`).then(function (result) {
+            ssh.dispose();
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    }).catch(function (err) {
+        done(err);
+    });
+});
