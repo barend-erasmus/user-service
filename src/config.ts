f931f4459060f87140b9592bbac39bd1e8c55c24
Barend Erasmus <developersworkspace@gmail.com>
2017-06-02 13:27:48 +0200

    fixes

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export let config = {
     db: {
-        uri: 'mongodb://localhost:27017/featuretoggledb'
+        uri: 'mongodb://localhost:27017/featuretoggledb',
     },
     emailOptions: {
         address: "5 BOLINGBROOK, SIR GEORGE GREY STREET, ORANJEZICHT, 8001",
@@ -13,6 +13,6 @@ export let config = {
     },
     secret: 'hello world',
     sendGrid: {
-        apiKey: '0a139705ddafc9ffc2a1f415d176175e68ab70f1acced808f405969f69781a4629b48d8abd690b8d077f175b5928b9c7f1acfca6f21af5eac117b4ead727dc54181f9dcca2'
+        apiKey: '0a139705ddafc9ffc2a1f415d176175e68ab70f1acced808f405969f69781a4629b48d8abd690b8d077f175b5928b9c7f1acfca6f21af5eac117b4ead727dc54181f9dcca2',
     },
 };
